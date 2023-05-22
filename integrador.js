ddc95169f207f087ceef29c2b7ab3417269d9fe6
Lara <laramolteni2901@gmail.com>
2023-05-21 21:44:00 -0300

    incorporacion del tp javascript

--- integrador.js
+++ integrador.js
@@ -1,13 +1,27 @@
 function precioFinal(){
-    let valor = 1500;
+    let valor = 200;
 
     let cantEnt = document.getElementById("cantidadEntradas").value;
     
     let desc = document.getElementById("descuento").value;
-    console.log(desc);
+    
     let porcDesc = ((cantEnt*valor)* desc)/100;
 
     let total = (cantEnt*valor) - porcDesc;
 
     document.getElementById("total").value = total;
 }
+
+function borrar(){
+    
+    document.getElementById("nombre").value="";
+    document.getElementById("apellido").value="";
+    document.getElementById("email").value="";
+    document.getElementById("cantidadEntradas").value="";
+    document.getElementById("descuento").value="";
+    document.getElementById("total").value="";
+
+}
+
+
+
